72eb9aabf0bea2495cfd3349c0f3255380be03b1
Rakib86547 <contact.antor.bd@gmail.com>
2022-10-25 23:52:47 +0600

    login and sign up from create

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -3,12 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../pages/share/Footer/Footer';
 import Header from '../pages/share/Header/Header';
 import LeftSideNav from '../pages/share/LeftSideNav/LeftSideNav';
+import main from './Main.css';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <div className='grid grid-cols-[1fr 3fr]'>
+            <div className='home-container px-4'>
                 <div>
                     <LeftSideNav></LeftSideNav>
                 </div>
